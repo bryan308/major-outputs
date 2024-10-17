7ff3e4fb02bb627024a8482cd031f83291bf26af
bryan308 <cdrcangulo@gmail.com>
2024-10-17 23:13:58 +0800

    feat: dynamic page using nested dynamic route for better performace
    * new subject included making it as my outputs on major sub soon
    
    * more old song soundtrip :)

---
+++ app/(labs)/[subject]/[slug]/page.tsx
@@ -0,0 +1,101 @@
+import React from "react"
+import { allItwst01s, allItwst02s, allItpf01s } from "content-collections"
+import { MDXContent } from "@content-collections/mdx/react"
+import { notFound } from "next/navigation"
+import Link from "next/link"
+import { Separator } from "@/components/ui/separator"
+import { Metadata } from "next"
+import FileCard from "@/components/shared/file-card"
+
+// fumadocs components
+import { ImageZoom } from "fumadocs-ui/components/image-zoom"
+import { Pre, CodeBlock } from "fumadocs-ui/components/codeblock"
+import { File, Folder, Files } from "fumadocs-ui/components/files"
+import { Tab, Tabs } from "fumadocs-ui/components/tabs"
+
+type Meta = {
+	path: string
+}
+
+type Lab = {
+	_meta: Meta
+	title: string
+	mdx: string
+}
+
+type SubjectKeys = "itwst01" | "itwst02" | "itpf01"
+
+const subjectData: Record<SubjectKeys, Lab[]> = {
+	itwst01: allItwst01s,
+	itwst02: allItwst02s,
+	itpf01: allItpf01s,
+}
+
+export default async function Page({ params }: { params: { subject: SubjectKeys; slug: string } }) {
+	const { subject, slug } = params
+	const labs = subjectData[subject] // No error here
+	const labItem = labs.find((lab) => lab._meta.path === slug)
+
+	if (!labItem) {
+		return notFound()
+	}
+
+	return (
+		<>
+			<Link href={`/${subject}`}>&larr; back</Link>
+			<h2 className="text-lg tracking-tighter my-6 font-semibold">
+				{subject.toUpperCase()} / {labItem.title}
+			</h2>
+			<MDXContent
+				code={labItem.mdx}
+				components={{
+					pre: (props) => (
+						<CodeBlock {...props}>
+							<Pre>{props.children}</Pre>
+						</CodeBlock>
+					),
+					Image: (props) => (
+						<ImageZoom
+							{...props}
+							className="w-full block mx-auto mt-4 rounded-lg"
+						/>
+					),
+					FileCard,
+					File,
+					Folder,
+					Files,
+					Tab,
+					Tabs,
+					hr: (props) => (
+						<Separator
+							{...props}
+							className="my-4"
+						/>
+					),
+				}}
+			/>
+			<div className="mb-10 lg:mb-20" />
+		</>
+	)
+}
+
+export const generateStaticParams = async () => {
+	const params = []
+	for (const subject in subjectData) {
+		const labs = subjectData[subject as SubjectKeys]
+		const slugs = labs.map((lab) => ({ subject, slug: lab._meta.path }))
+		params.push(...slugs)
+	}
+	return params
+}
+
+export function generateMetadata({ params }: { params: { subject: SubjectKeys; slug: string } }) {
+	const { subject, slug } = params
+	const labs = subjectData[subject]
+	const page = labs.find((lab) => lab._meta.path === slug)
+
+	return {
+		title: `${page?.title} | ${subject.toUpperCase()}`,
+		// Add additional metadata fields as needed
+	} satisfies Metadata
+}
--- components/shared/file-card.tsx
+++ components/shared/file-card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
+import { File } from "@mynaui/icons-react"
 
 function FileCard({
 	fileName,
@@ -14,12 +15,16 @@ function FileCard({
 	return (
 		<div className="border flex max-w-72 rounded-lg">
 			<div className="border-r grid place-items-center">
-				<Image
-					src={image}
-					alt="pdf thumbnail"
-					width={56}
-					height={56}
-				/>
+				{image ? (
+					<Image
+						src={image}
+						alt="pdf thumbnail"
+						width={56}
+						height={56}
+					/>
+				) : (
+					<File className="size-6 m-4" />
+				)}
 			</div>
 			<div className="p-2">
 				<Link

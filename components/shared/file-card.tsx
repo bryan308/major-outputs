0586747f71d210f7f0174aaf5a328a6b8f3d3712
bryan308 <cdrcangulo@gmail.com>
2024-11-23 18:31:58 +0800

    feat: update file card component styles and improve case scores in laboratory documents

--- components/shared/file-card.tsx
+++ components/shared/file-card.tsx
@@ -13,20 +13,21 @@ function FileCard({
 	filePath: string
 }) {
 	return (
-		<div className="border flex max-w-72 rounded-lg">
+		<div className="border flex max-w-80 max-h-14 my-4 rounded-lg overflow-hidden">
 			<div className="border-r grid place-items-center">
 				{image ? (
 					<Image
 						src={image}
-						alt="pdf thumbnail"
+						alt={fileName}
 						width={56}
 						height={56}
+						className="bg-cover min-w-14"
 					/>
 				) : (
 					<File className="size-6 m-4" />
 				)}
 			</div>
-			<div className="p-2">
+			<div className="p-2 overflow-hidden truncate">
 				<Link
 					href={filePath}
 					target="_blank"
